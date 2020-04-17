02340dd6f84f734f0fd55151bf25584ed4cb9a6b
arabind-meher <arabindmeher.99@gmail.com>
2020-04-17 18:43:43 +0530

    commit

--- main.py
+++ main.py
@@ -5,20 +5,32 @@ import cv2
 from tkinter import *
 from tkinter import ttk, filedialog
 
+IMAGE_EXTENSIONS = ['bmp', 'pbm', 'pgm', 'ppm', 'sr', 'ras', 'jpeg', 'jpg', 'jpe', 'jp2', 'tiff', 'tif', 'png']
+
 
 class ImageResize:
     """Image Resize"""
 
+    # Resize Image to a given Ratio.
     @staticmethod
     def image_resize_ratio(path, ratio):
+        # Create an 'Output' Directory path
         output_path = join(path, 'Output')
+
+        # Check if the Directory already existed
         if not exists(output_path):
+            # Create Directory
             os.mkdir(output_path)
             print('Directory Created')
         else:
             print('Directory Already Exist')
 
-        files = [file for file in listdir(path) if isfile(join(path, file)) and file[0] != '.']
+        # List all the files present in the Directory
+        files = [file for file in listdir(path)
+                 if isfile(join(path, file)) and file[0] != '.' and file.split('.')[-1] in IMAGE_EXTENSIONS]
+        print(files)
+
+        # Convert each file one-by-one
         for file in files:
             image = cv2.imread(join(path, file), cv2.IMREAD_UNCHANGED)
             h, w, d = image.shape
@@ -28,18 +40,26 @@ class ImageResize:
             resized_image = cv2.resize(image, dimension, interpolation=cv2.INTER_CUBIC)
             print(resized_image.shape)
 
+            # Save the Resized Image to 'Output' Directory
             cv2.imwrite(join(output_path, file), resized_image)
 
+    # Resize Image to given Width and Height
     @staticmethod
     def image_resize_pixel(path, width, height):
+        # Create an 'Output' Directory path
         output_path = join(path, 'Output')
+
+        # Check if the Directory already existed
         if not exists(output_path):
+            # Create Directory
             os.mkdir(output_path)
             print('Directory Created')
         else:
             print('Directory Already Exist')
 
-        files = [file for file in listdir(path) if isfile(join(path, file)) and file[0] != '.']
+        # List all the files present in the Directory
+        files = [file for file in listdir(path)
+                 if isfile(join(path, file)) and file[0] != '.' and file.split('.')[-1] in IMAGE_EXTENSIONS]
         for file in files:
             image = cv2.imread(join(path, file), cv2.IMREAD_UNCHANGED)
             print(image.shape)
@@ -55,6 +75,7 @@ class GUI:
     """Graphics User Interface"""
 
     def __init__(self):
+        # Create a Graphics Window
         root = Tk()
         root.title('Image Resize')
         root.geometry('500x500')
@@ -75,24 +96,24 @@ class GUI:
             font=('bitstream vera sans', 12)
         ).place(x=10, y=70)
 
-        folder = StringVar()
+        directory = StringVar()
         Entry(
             root,
-            textvariable=folder,
+            textvariable=directory,
             background='white',
             font=('bitstream vera sans', 12),
             width=32
         ).place(x=125, y=70)
 
-        def find_folder():
-            file_location = filedialog.askdirectory(initialdir='/home', title='Select Folder')
-            folder.set(file_location)
+        def find_directory():
+            file_location = filedialog.askdirectory(initialdir='/home', title='Select directory')
+            directory.set(file_location)
 
         Button(
             root,
             text='...',
             font=('bitstream vera sans', 12),
-            command=find_folder
+            command=find_directory
         ).place(x=455, y=65)
 
         ttk.Separator(root).place(y=125, relwidth=1)
@@ -120,7 +141,7 @@ class GUI:
 
         def convert_ratio():
             ImageResize.image_resize_ratio(
-                folder.get(),
+                directory.get(),
                 float(ratio.get())
             )
 
@@ -165,7 +186,7 @@ class GUI:
 
         def convert_pixel():
             ImageResize.image_resize_pixel(
-                folder.get(),
+                directory.get(),
                 int(width.get()),
                 int(height.get()),
             )
